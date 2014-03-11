11d20fa0c58ea31330e0bd42af87dac7d803a986
IliyaBoyanov <iliq9204@gmail.com>
2014-03-11 16:00:09 +0200

    .init

--- CSharpPartOne/IntroProgrammingHomework/15AgeAfterTenYears/AgeAfterTenYears.cs
+++ CSharpPartOne/IntroProgrammingHomework/15AgeAfterTenYears/AgeAfterTenYears.cs
@@ -4,12 +4,13 @@ class AgeAfterTenYears
 {
     static void Main()
     {
+        Console.Write("Enter your birhday: ");
         DateTime birthDay = new DateTime();
         birthDay = DateTime.Parse(Console.ReadLine());
         System.TimeSpan myCurrentAge = new TimeSpan();
-        myCurrentAge = (DateTime.Parse(Console.ReadLine()) - birthDay);
+        myCurrentAge = (DateTime.Now - birthDay);
         int age = (int)(myCurrentAge.Days / 365.2 + 10);
-        Console.WriteLine(age);
+        Console.WriteLine("Your age after ten years will be: "+ age);
     }
 }
 
